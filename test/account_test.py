a8f7520c37f75e098623e58e269d3fccf11cb205
agiledragon <zhangxiaolong1980@126.com>
2019-07-28 08:12:30 +0800

    refactor

--- test/account_test.py
+++ test/account_test.py
@@ -33,41 +33,39 @@ class FakeAccountRepo(Repo):
 repo = FakeAccountRepo()
 local_account_repo.set(repo)
 
+JIM_PHONE_NUMBER = "19999999999"
+JIM_INIT_AMOUNT = 10000
+LUCY_PHONE_NUMBER = "18888888888"
+LUCY_INIT_AMOUNT = 5000
+AMOUNT = 1500
+
 
 class AccountTest(unittest.TestCase):
 
     def setUp(self):
         self.api = AccountApi()
-        self.jim_phone_number = "19999999999"
-        self.jim_init_amount = 10000
-        self.jim_account_id = self.api.create_account(self.jim_phone_number, self.jim_init_amount)
-        lucy_phone_number = "18888888888"
-        self.lucy_init_amount = 5000
-        self.lucy_account_id = self.api.create_account(lucy_phone_number, self.lucy_init_amount)
+        self.jim_account_id = self.api.create_account(JIM_PHONE_NUMBER, JIM_INIT_AMOUNT)
+        self.lucy_account_id = self.api.create_account(LUCY_PHONE_NUMBER, LUCY_INIT_AMOUNT)
 
     def tearDown(self):
         self.api.destroy_account(self.jim_account_id)
 
     def test_withdraw_money(self):
-        amount = 1500
-        self.api.withdraw_money(self.jim_account_id, amount)
-        self.assertEqual(self.jim_init_amount - amount, self.api.get_amount(self.jim_account_id))
+        self.api.withdraw_money(self.jim_account_id, AMOUNT)
+        self.assertEqual(JIM_INIT_AMOUNT - AMOUNT, self.api.get_amount(self.jim_account_id))
 
     def test_transfer_money_to_local_service(self):
-        amount = 1500
-        self.api.transfer_money_to_local(self.jim_account_id, self.lucy_account_id, amount)
-        self.assertEqual(self.jim_init_amount - amount, self.api.get_amount(self.jim_account_id))
-        self.assertEqual(self.lucy_init_amount + amount, self.api.get_amount(self.lucy_account_id))
+        self.api.transfer_money_to_local(self.jim_account_id, self.lucy_account_id, AMOUNT)
+        self.assertEqual(JIM_INIT_AMOUNT - AMOUNT, self.api.get_amount(self.jim_account_id))
+        self.assertEqual(LUCY_INIT_AMOUNT + AMOUNT, self.api.get_amount(self.lucy_account_id))
 
     def test_transfer_money_to_remote_service(self):
-        amount = 1500
-        self.api.transfer_money_to_remote(self.jim_account_id, self.lucy_account_id, amount)
-        self.assertEqual(self.jim_init_amount - amount, self.api.get_amount(self.jim_account_id))
+        self.api.transfer_money_to_remote(self.jim_account_id, self.lucy_account_id, AMOUNT)
+        self.assertEqual(JIM_INIT_AMOUNT - AMOUNT, self.api.get_amount(self.jim_account_id))
 
     def test_transfer_money_from_remote_service(self):
-        amount = 1500
-        self.api.transfer_money_from_remote(self.lucy_account_id, self.jim_account_id, amount)
-        self.assertEqual(self.jim_init_amount + amount, self.api.get_amount(self.jim_account_id))
+        self.api.transfer_money_from_remote(self.lucy_account_id, self.jim_account_id, AMOUNT)
+        self.assertEqual(JIM_INIT_AMOUNT + AMOUNT, self.api.get_amount(self.jim_account_id))
 
 
 if __name__ == '__main__':
